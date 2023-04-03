13c519434e55a48a6d7e2b35d4a4983f9520d6a1
murarisahdev <murarisahdev@gmail.com>
2023-04-03 18:27:07 +0530

    add the image function

---
+++ src/utils/uniqueCards.js
@@ -0,0 +1,30 @@
+export const uniqueCards = [
+  {
+    type: "Butterfly",
+    image: require(`../assets/images/butterfly.jpeg`),
+  },
+  {
+    type: "Cat",
+    image: require(`../assets/images/cat.jpeg`),
+  },
+  {
+    type: "Dog",
+    image: require(`../assets/images/dog.jpeg`),
+  },
+  {
+    type: "Fish",
+    image: require(`../assets/images/fish.jpeg`),
+  },
+  {
+    type: "Mockey",
+    image: require(`../assets/images/mockey.jpeg`),
+  },
+  {
+    type: "Parrot",
+    image: require(`../assets/images/parrot.jpeg`),
+  },
+  {
+    type: "Zebra",
+    image: require(`../assets/images/zebra.jpeg`),
+  },
+];
