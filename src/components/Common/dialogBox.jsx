e0aa70aaa5836c4ab92fe80b404d677573013e13
murarisahdev <murarisahdev@gmail.com>
2023-04-04 12:02:36 +0530

    Create the common components

---
+++ src/components/Common/dialogBox.jsx
@@ -0,0 +1,37 @@
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  Button,
+  DialogTitle,
+} from "@mui/material";
+
+const DialogBox = ({ showModal, moves, getScore, handleRestart }) => {
+  return (
+    <Dialog
+      open={showModal}
+      disableBackdropClick
+      disableEscapeKeyDown
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <DialogTitle id="alert-dialog-title">
+        Hurray!!! You completed the challenge
+      </DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          You completed the game in {moves} moves. Your score is{" "}
+          {getScore() ?? "0"}
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleRestart} color="primary">
+          Restart
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default DialogBox;
---
+++ src/components/Common/selectOption.jsx
@@ -0,0 +1,30 @@
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+
+const SelectOption = ({ option,  setOption }) => {
+
+  const handleChange = (event) => {
+    setOption(event.target.value);
+  };
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }}>
+      <Select
+        value={option}
+        onChange={handleChange}
+        displayEmpty
+        inputProps={{ "aria-label": "Without label" }}
+      >
+        <MenuItem value={10}>
+          <em>select pair</em>
+        </MenuItem>
+        <MenuItem value={6}>6 pairs</MenuItem>
+        <MenuItem value={8}>8 pairs</MenuItem>
+        <MenuItem value={10}>10 pairs </MenuItem>
+      </Select>
+    </FormControl>
+  );
+};
+
+export default SelectOption;
