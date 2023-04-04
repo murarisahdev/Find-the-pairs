fec8aa5f529dc01ff46b445ad78d7676822eebd8
murarisahdev <murarisahdev@gmail.com>
2023-04-04 14:38:47 +0530

    Fix the bugs for images

--- src/utils/uniqueCards.js
+++ src/utils/uniqueCards.js
@@ -4,6 +4,10 @@ export const uniqueCardsArray = [
     image: require(`../assets/images/butterfly.jpeg`),
   },
   {
+    type: "fish",
+    image: require(`../assets/images/fish.jpeg`),
+  },
+  {
     type: "Cat",
     image: require(`../assets/images/cat.jpeg`),
   },
@@ -12,8 +16,8 @@ export const uniqueCardsArray = [
     image: require(`../assets/images/dog.jpeg`),
   },
   {
-    type: "Fish",
-    image: require(`../assets/images/fish.jpeg`),
+    type: "Lion",
+    image: require(`../assets/images/lion.jpeg`),
   },
   {
     type: "Mockey",
@@ -27,4 +31,12 @@ export const uniqueCardsArray = [
     type: "Zebra",
     image: require(`../assets/images/zebra.jpeg`),
   },
+  {
+    type: "Horse",
+    image: "http://t2.gstatic.com/licensed-image?q=tbn:ANd9GcRJ_M148rK8--5Yg5_0gEw49cjcIhQvvh4vv8xdgzMmNYFlqnXIHZ7BDCWEKplKd5-VcsOlHKVEfhUnrpU"
+  },
+  {
+    type: "Elephant",
+    image: "https://files.worldwildlife.org/wwfcmsprod/images/African_Elephant_Kenya_112367/story_full_width/qxyqxqjtu_WW187785.jpg",
+  }
 ];
